97c0b54dafcae4be2b1a67c9ebfa709e1fdd71f3
Florin Pățan <florinpatan@gmail.com>
2026-07-09 22:33:26 +0300

    7: add random package and GET /random/number endpoint

--- httpapi/handler.go
+++ httpapi/handler.go
@@ -4,8 +4,10 @@ package httpapi
 import (
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/dlsniper/test-workflow"
+	"github.com/dlsniper/test-workflow/random"
 )
 
 // Handler returns the HTTP handler for the hello API.
@@ -14,10 +16,16 @@ func Handler() http.Handler {
 	mux.HandleFunc("GET /hello/{name}", hello)
 	mux.HandleFunc("GET /hello/{$}", hello)
 	mux.HandleFunc("GET /hello", hello)
+	mux.HandleFunc("GET /random/number", randomNumber)
 	return mux
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	io.WriteString(w, greeting.Hello(r.PathValue("name")))
+}
+
+func randomNumber(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	io.WriteString(w, strconv.Itoa(random.Number()))
 }
---
+++ random/random_test.go
@@ -0,0 +1,12 @@
+package random
+
+import "testing"
+
+func TestNumber(t *testing.T) {
+	for range 1000 {
+		n := Number()
+		if n < 0 || n > 1_000_000 {
+			t.Fatalf("Number() = %d, out of range [0, 1000000]", n)
+		}
+	}
+}
